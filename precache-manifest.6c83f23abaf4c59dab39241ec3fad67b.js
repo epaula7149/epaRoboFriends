a78423199acb042d91b2603935517e1fe42853f7
epaula100721 <elwynanderson@elwyns-iMac.local>
2021-10-13 12:48:45 -0500

    Updates

---
+++ precache-manifest.6c83f23abaf4c59dab39241ec3fad67b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd48d11ff2bb7264ab555b8540e34e33",
+    "url": "/epaRoboFriends/index.html"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/epaRoboFriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "5d2b682310c895eef89e",
+    "url": "/epaRoboFriends/static/css/main.6bc1731a.chunk.css"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/epaRoboFriends/static/js/2.e1b685fc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/epaRoboFriends/static/js/2.e1b685fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2b682310c895eef89e",
+    "url": "/epaRoboFriends/static/js/main.f1dee615.chunk.js"
+  },
+  {
+    "revision": "ca335e895fd17b2a2399",
+    "url": "/epaRoboFriends/static/js/runtime-main.a9f1c832.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/epaRoboFriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
